c0532ff493b3b2de06b67e5886bcdace7ac2a35b
dyw-vince <2036105396@qq.com>
2025-06-23 16:50:59 +0800

    assignment8

--- eighth_assignment/main.py
+++ eighth_assignment/main.py
@@ -1,10 +1,12 @@
 from train import *
 from test import *
 import yaml
+os.environ["http_proxy"] = "http://10.54.14.112:7890"
+os.environ["https_proxy"] = "http://10.54.14.112:7890"
 if __name__ == '__main__':
     with open("config.yaml", "r") as f:
         config = yaml.safe_load(f)
-    runner = Training(config,test=False)
-    runner.run()
+    # runner = Training(config,test=False)
+    # runner.run()
     tester= Testing(config,test=True)
     tester.run()
