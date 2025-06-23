d0500b5802beef4d5ba86ff31f103a56846f13e9
dyw-vince <2036105396@qq.com>
2025-06-23 17:43:24 +0800

    assignment10_v1

---
+++ assignment10/my_maddpg/agents/maddpg_agents.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+from argparse import Namespace
+from my_maddpg.environment import DummyVecMultiAgentEnv  
+from my_maddpg.agents.iddpg_agents import IDDPG_Agents
+from my_maddpg.policies.deterministic_marl import MADDPG_Policy
+
+
+class MADDPG_Agents(IDDPG_Agents):
+
+    def __init__(self,
+                 config: Namespace,
+                 envs):
+        super(MADDPG_Agents, self).__init__(config, envs)
+
+    def _build_policy(self):
+        normalize_fn = None
+        initializer = torch.nn.init.orthogonal_
+        activation = nn.LeakyReLU
+        device = self.device
+
+        # build representations
+        A_representation = self._build_representation(self.observation_space)
+        critic_in = [sum(self.observation_space[k].shape) +
+                     sum(self.action_space[k].shape) for k in self.agent_keys]
+        space_critic_in = {k: (sum(critic_in), ) for k in self.agent_keys}
+        C_representation = self._build_representation(space_critic_in)
+
+        # build policies
+        policy = MADDPG_Policy(
+            action_space=self.action_space, n_agents=self.n_agents,
+            actor_representation=A_representation, critic_representation=C_representation,
+            actor_hidden_size=self.config.actor_hidden_size,
+            critic_hidden_size=self.config.critic_hidden_size,
+            normalize=normalize_fn, initialize=initializer, activation=activation,
+            activation_action=nn.Sigmoid,
+            device=device,
+            use_parameter_sharing=self.use_parameter_sharing,
+            model_keys=self.model_keys,
+        )
+        self.continuous_control = True
+
+        return policy
